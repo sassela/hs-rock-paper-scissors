44d35646c6e2b5a300a5ff68c7b6fd50fc3c753b
sassela <sassela@users.noreply.github.com>
2020-11-29 17:52:36 +0000

    Add picture

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,9 @@ import           Data.Text (Text, pack)
 import           Lib (randomMove)
 import           System.Random (Random (randomIO))
 
+picture :: Text -> Picture
+picture state = lettering state
+
 change :: Event -> Text -> Text
 change event state = state
 
